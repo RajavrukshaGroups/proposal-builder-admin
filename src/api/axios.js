7cb97912b2d16bf29d577a957fab567d349f5a0d
anupsr <sranup007@gmail.com>
2026-06-12 16:00:43 +0530

    new changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_URL || "https://server.bouncyboxstudio.in",
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:12000",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://proposalserver.digitaleliteservices.in",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
